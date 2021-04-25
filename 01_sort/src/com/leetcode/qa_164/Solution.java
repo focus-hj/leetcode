e336d4a7c2c59b100a2ca452e0feb451522688ed
huangjian <hj_0127@163.com>
2021-04-25 17:36:05 +0800

    feat:开始164题

---
+++ 01_sort/src/com/leetcode/qa_164/Solution.java
@@ -0,0 +1,37 @@
+package com.leetcode.qa_164;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个无序的数组，找出数组在排序之后，相邻元素之间最大的差值。
+ * 如果数组元素个数小于 2，则返回 0。
+ *
+ * 示例 1:
+ * 输入: [3,6,9,1]
+ * 输出: 3
+ * 解释: 排序后的数组是 [1,3,6,9], 其中相邻元素 (3,6) 和 (6,9) 之间都存在最大差值 3。
+ *
+ * 示例 2:
+ * 输入: [10]
+ * 输出: 0
+ * 解释: 数组元素个数小于 2，因此返回 0。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/maximum-gap
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author huangjian
+ */
+public class Solution {
+
+    public int maximumGap(int[] nums) {
+        if (nums.length < 2) {
+            return 0;
+        }
+        Arrays.sort(nums);
+        int max = 0;
+        for (int i = 0; i < nums.length - 1; i ++) {
+            max = Math.max(nums[i + 1] - nums[i], max);
+        }
+        return max;
+    }
+}
