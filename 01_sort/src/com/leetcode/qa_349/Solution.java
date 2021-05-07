107d78f00085c3b725e62fe0730e5bed5cbccab9
huangjian <hj_0127@163.com>
2021-05-07 10:18:29 +0800

    update:完成220,349,350

---
+++ 01_sort/src/com/leetcode/qa_349/Solution.java
@@ -0,0 +1,32 @@
+package com.leetcode.qa_349;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * 给定两个数组，编写一个函数来计算它们的交集。
+ *
+ * 示例 1：
+ * 输入：nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出：[2]
+ *
+ * 示例 2：
+ * 输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出：[9,4]
+ *  
+ * 说明：
+ * 输出结果中的每个元素一定是唯一的。
+ * 我们可以不考虑输出结果的顺序。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/intersection-of-two-arrays
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author huangjian
+ */
+public class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> nums1Set = Arrays.stream(nums1).boxed().collect(Collectors.toSet());
+        Set<Integer> nums2Set = Arrays.stream(nums2).boxed().collect(Collectors.toSet());
+        return nums1Set.stream().filter(nums2Set::contains).mapToInt(Integer::intValue).toArray();
+    }
+}
